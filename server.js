0027742b7515d9c74b41749e8684c3c34b17224b
利开团 <likaituan@wecash.net>
2017-08-30 17:33:30 +0800

    quick submit

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
-var config = {
-	serverPath: 'http://localhost:8008'
+var serverPath = 'http://localhost:8008';
+
+var proxyTable = {
+	'/service/mark': `${serverPath}/service/mark`
 };
 
 var express = require("express");
@@ -7,11 +9,13 @@ var app = express();
 
 app.use('/', express.static('.'));
 
-var proxyPath = require('http-proxy-middleware')(config.serverPath);
-app.use('/service', proxyPath);
+var proxy = require('http-proxy-middleware');
+Object.keys(proxyTable).forEach(key => {
+	app.use(key, proxy(proxyTable[key]));
+});
 
 var port = 3000;
 app.listen(port, x => {
 	console.log(`Node is Run At http://localhost:${port}`);
-	console.log(`Admin is Run At ${config.serverPath}/admin`);
+	console.log(`Admin is Run At ${serverPath}/admin`);
 });
